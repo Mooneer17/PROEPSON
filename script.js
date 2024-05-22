10075fa596787cda58a5b8aaa9c40e32d3480390
Mooneer17 <105901095+Mooneer17@users.noreply.github.com>
2024-05-22 22:29:43 +0000

    Refactor slider section in index.html and style.css

---
+++ script.js
@@ -0,0 +1,33 @@
+let currentIndex=0;
+
+const box = document.getElementsByClassName('slider-content'); 
+
+const handleNext = () => {
+    const nextIndex = currentIndex + 1 <= box.length - 1 ? currentIndex + 1 : 0;
+    const currentBox = document.querySelector(`[data-index="${currentIndex}"]`);
+    const nextBox = document.querySelector(`[data-index="${nextIndex}"]`);
+
+
+    currentBox.dataset.status = 'active';
+    currentBox.dataset.status = 'hide';
+    nextBox.dataset.status = 'next';
+
+
+    currentIndex = nextIndex;   
+
+}
+
+const handlePrev = () => {
+    const nextIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : box.length - 1;
+    const currentBox = document.querySelector(`[data-index="${currentIndex}"]`);
+    const nextBox = document.querySelector(`[data-index="${nextIndex}"]`);
+
+
+    currentBox.dataset.status = 'unknown';
+    currentBox.dataset.status = 'hide';
+    nextBox.dataset.status = 'next';
+
+
+    currentIndex = nextIndex;   
+
+}
